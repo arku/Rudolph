2da0e42cb6da147d3849249a8af9f485d46662ba
Flora Saramago <flora.saramago@gmail.com>
2015-03-28 14:48:55 -0300

    First part of Groups CRUD

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,5 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$('#group_date').fdatepicker();
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,33 @@ class GroupsController < ApplicationController
   before_action :authenticate_person!
 
   def index
+    @groups = current_person.groups
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      flash[:success] = "Successfully created group #{@group.name}"
+      redirect_to groups_path
+    else
+      flash[:error] = e.message
+      render 'new'
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:admin_id, :name, :description, :location, :date, :price_range)
   end
 
 end
