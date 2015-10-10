e9e905476f582da7410d2c4fa14b6d659d793d9f
Flora Saramago <flora.saramago@gmail.com>
2015-10-10 14:25:13 -0300

    Fix environment config

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rudolph::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['sendgrid_username'],
-  :password => ENV['sendgrid_password'],
-  :domain => 'itsrudolph.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+if Rails.env == 'production'
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['sendgrid_username'],
+    :password => ENV['sendgrid_password'],
+    :domain => 'itsrudolph.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
