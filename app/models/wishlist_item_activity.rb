3f9d74e47cf18c212661206f7c47c2bde29de9bd
Flora Saramago <flora.saramago@gmail.com>
2015-10-19 22:54:43 -0200

    Limit wishlist item name in activity feed

--- app/models/wishlist_item_activity.rb
+++ app/models/wishlist_item_activity.rb
@@ -9,10 +9,17 @@ class WishlistItemActivity < GroupActivity
   end
 
   def description
-    I18n.t('wishlist_item_activity', person: person.first_name, item: wishlist_item.name)
+    I18n.t('wishlist_item_activity', person: person.first_name, item: item_name_short)
   end
 
   def full_description
-    I18n.t('wishlist_item_activity_full', person: person.first_name, item: wishlist_item.name, group: group.name)
+    I18n.t('wishlist_item_activity_full', person: person.first_name, item: item_name_short, group: group.name)
+  end
+
+  private
+
+  def item_name_short
+    name = wishlist_item.name
+    name.size <= 70 ? name : "#{name[0..69]}..."
   end
 end
